14b595f25b59eaa044dcc619dc4922dcb55602c5
igornogueira2014 <igornogueira2014@gmail.com>
2024-07-22 11:28:52 -0300

    22-07-24_2

--- tipo-number.js
+++ tipo-number.js
@@ -1,1 +1,10 @@
-console.log('Hello Wolrd!!');
+const notaPrimeiroBi = 8;
+const notaSegundoBi = 6.3;
+const notaTerceiroBi = -2;
+const notaQuartoBi = Number.parseInt('5');
+
+const total = (notaPrimeiroBi + notaSegundoBi + notaTerceiroBi + notaQuartoBi) / 4;
+
+console.log(total);
+
+console.log('a média é ' + total.toFixed(2));
