da170a00d253764b97dfa13efa25c625c93fd565
igornogueira2014 <igornogueira2014@gmail.com>
2024-07-27 12:51:23 -0300

    27-07-24_1

---
+++ tipoBoolean.js
@@ -0,0 +1,14 @@
+const estudante = 'Fernando';
+const estaAprovado = true;
+
+if (estaAprovado === true) {
+    console.log('Parabens, boas festas!');
+} else {
+    console.log('REPROVADO!,Boas festas');
+}
+
+if (estudante === 'Fernando') {
+    console.log(`Olá. ${estudante}`);
+} else {
+    console.log('Quem é vc?');
+}
