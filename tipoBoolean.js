3e99bb1274b099c9ee181cc661063a698c615f1a
igornogueira2014 <igornogueira2014@gmail.com>
2024-07-27 13:32:25 -0300

    27-07-24_2

--- tipoBoolean.js
+++ tipoBoolean.js
@@ -1,14 +1,19 @@
 const estudante = 'Fernando';
 const estaAprovado = true;
 
+//Exemplo 1
+/*
 if (estaAprovado === true) {
     console.log('Parabens, boas festas!');
 } else {
     console.log('REPROVADO!,Boas festas');
 }
+*/
+
+//Exemplo 2
 
 if (estudante === 'Fernando') {
     console.log(`Olá. ${estudante}`);
 } else {
-    console.log('Quem é vc?');
+    console.log('Quem é vc?')
 }
---
+++ tipoNullUndefined.js
@@ -0,0 +1,10 @@
+// null significa nulo, inexistente,
+// NAN  significa tipo numerico, em que foi realizado uma operação numérica , mau formulada!.
+// typeof retorna, de que tipo é a variável.
+
+const professora = 'Ana';
+let telefoneEstudante = null;
+
+
+console.log(typeof professora);
+console.log( typeof telefoneEstudante);
