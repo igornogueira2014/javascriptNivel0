238ea1bb2d3fb37399bda614add141e3804de457
igornogueira2014 <igornogueira2014@gmail.com>
2024-08-02 10:54:29 -0300

    02-08-24_1

---
+++ operadoresLogicos.js
@@ -0,0 +1,8 @@
+const notaFinal = 6;
+const faltas = 5;
+
+if (notaFinal < 7 && faltas > 4) {
+    console.log('Reprovado, boas  festas');
+} else {
+    console.log('Não foi reprovado por faltas');
+}
